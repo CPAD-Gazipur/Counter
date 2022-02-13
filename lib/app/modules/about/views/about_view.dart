d86eca94a0e62076c053a4607a2b396a8b255937
Your Name <you@example.com>
2022-02-14 00:00:00 +0600

    Ui Updated by Yeaseen

--- lib/app/modules/about/views/about_view.dart
+++ lib/app/modules/about/views/about_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:user_profile_avatar/user_profile_avatar.dart';
 
 import '../controllers/about_controller.dart';
@@ -13,30 +14,127 @@ class AboutView extends GetView<AboutController> {
         title: Text(
           "Yeaseen Ahmed's Information",
           style: TextStyle(
-            color: Color(0xff007200),
+            color: Color(0xffffffff),
           ),
         ),
         centerTitle: true,
-        backgroundColor: Color(0xffffffff),
-        shadowColor: Color(0xffffffff),
+        backgroundColor: Color(0xff007200),
+        shadowColor: Color(0xff007200),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            UserProfileAvatar(
-              avatarUrl:
-                  'G:/git repository/CPAD-Gazipur/Counter/assets/WIN_20201222_11_34_21_Pro (2).jpg',
-              avatarSplashColor: Color(0xff007200),
-              radius: 100,
-              isActivityIndicatorSmall: false,
-              avatarBorderData: AvatarBorderData(
-                borderColor: Colors.white,
-                borderWidth: 5.0,
-              ),
-            )
-          ],
-        ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Text(
+                  "Web Developer::[Yeaseen Ahmed]",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Roboto',
+                    letterSpacing: 1.5,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                UserProfileAvatar(
+                  avatarUrl:
+                      'https://avatars.githubusercontent.com/u/92775489?v=4',
+                  avatarSplashColor: Color(0xff007200),
+                  radius: 100,
+                  isActivityIndicatorSmall: false,
+                  avatarBorderData: AvatarBorderData(
+                    borderColor: Colors.white,
+                    borderWidth: 5.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Yeaseen Ahmed",
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff007200),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.email,
+                              color: Color(0xff007200),
+                              size: 30,
+                            ),
+                            Text(
+                              " shekhyeaseen@gmail.com",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.phone,
+                              color: Color(0xff007200),
+                              size: 30,
+                            ),
+                            Text(
+                              "01720637100 , 01712700477",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.location_on,
+                              color: Color(0xff007200),
+                              size: 30,
+                            ),
+                            Text(
+                              "Hakkani,Gazipur,Bangladesh",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
